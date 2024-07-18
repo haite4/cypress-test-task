6b2e2547c870ea6233db76ba1f4c4a6d16198eff
haite4 <ugrayvcsgo137@gmail.com>
2024-07-18 14:33:42 +0100

    Updated cypress-test.yml

--- cypress/config/cypress-mobile.config.js
+++ cypress/config/cypress-mobile.config.js
@@ -1,9 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "y5nty9",
   viewportWidth: 375,
   viewportHeight: 667,
-
   reporter: "cypress-mochawesome-reporter",
   e2e: {
     baseUrl: "https://demo.realworld.io/#",
