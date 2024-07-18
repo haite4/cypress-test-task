d0ea0555bcf588de6f777d195f6e0cdd7dbd87b9
haite4 <ugrayvcsgo137@gmail.com>
2024-07-18 14:44:46 +0100

    Updated yml file

--- cypress/config/cypress-mobile.config.js
+++ cypress/config/cypress-mobile.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "y5nty9",
   viewportWidth: 375,
   viewportHeight: 667,
   reporter: "cypress-mochawesome-reporter",
